0d9dc6d61f00c82487b5c641a239f1dd40ac6666
Renée Cruz <reneebasscruz@gmail.com>
2019-01-16 07:00:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,33 +13,35 @@ function logShout(string)
       console.log(string.toUpperCase())
 }
 
-function logWhisper(string)
-  {
+function logWhisper(string) {
     console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string)
-  {
-    
-      var lowercase = string.toLowerCase
-      var uppercase = string.toUpperCase
-      
-      if 
-      lowercase.toLowerCase() === lowercase {
+function sayHiToGrandma(string) {
+      if (string.toLowerCase() === string) {
       return "I can\'t hear you!"
-        
       }
       
-      else if
-      uppercase.toUpperCase() === uppercase{
+      if (string === string) {
       return "YES, INDEED!"
-        
       }
       
-      else
-      
-      string === "I love you, Grandma" {
+      if (string === "I love you, Grandma.") {
       return "I love you, too.""
       }
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+}
